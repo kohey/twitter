d786eae5ba662b9f677cefe08b6313d3bd131f35
kohey <k_kawamura@kiizan-kiizan.co.jp>
2019-02-25 16:08:27 +0900

    文字数制限、collectiveの追加、新規登録機能をつけました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 <div class="center jumbotron">
     <div class="text-center">
         <h1>Laravel基礎から応用へ進むための素材です</h1>
+        {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
     </div>
 </div>
 @endsection 
