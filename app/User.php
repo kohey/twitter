93fe98eb9de60b89e55c52e2e983e1765e6cb448
kohey <k_kawamura@kiizan-kiizan.co.jp>
2019-02-26 18:14:38 +0900

    フォロー・アンフォローをする時は2つの確認が必要です. また、belongsToManyも実装しました

--- app/User.php
+++ app/User.php
@@ -31,4 +31,50 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
+
+    //多対多の宣言
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+
+    //フォロー・アンフォローの定義(フォロー確認のメソッドも定義)
+    public function follow($userId)
+    {
+        $exist = $this->is_following($userId);
+        $its_me = $this->id == $userId;
+
+        if ($exist || $its_me) {
+            return false;
+        } else {
+            $this->followings()->attach($userId);
+
+            return true;
+        }
+    }
+
+    public function unfollow($userId)
+    {
+        $exist = $this->is_following($userId);
+        $its_me = $this->id == $userId;
+
+        if ($exist && !$its_me) {
+            return false;
+        } else {
+            $this->following()->detach($userId);
+
+            return true;
+        }
+    }
+
+    //現在フォローしているかしていないかの判定
+    public function is_following($userId)
+    {
+        $this->following()->where('follow_id', $userId)->exists();
+    }
 }
